ddaf4507b59bdc246ed9b99970fc18afa0d078e4
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-13 11:48:09 +0900

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,1 @@
---disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RAeeH5SQ4Cq2GJsN1M7QwFvL2oqPu6M88r.azure --password x
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RAeeH5SQ4Cq2GJsN1M7QwFvL2oqPu6M88r.azure --password x
